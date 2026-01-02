ccf48d73568e388ea7402a06d66abd18b2e8de38
md-afan <mdafan5454@gmail.com>
2026-01-02 14:52:53 +0530

    Updated UI and bug fixes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:process.env.VITE_BASE_PATH || "/QuizGen_AI",
+   define: {
+    'process.env': {},
+    'global': {}
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  }
 })
